ec1b2612d8cfbb8d306f2c683d2dd3579fbaaa59
edabarutcu <edabarutcu@protonmail.com>
2022-07-31 18:24:45 +0300

    Configuration setting

---
+++ SnapKit-TimerApp/ExtensionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExtensionViewController.swift
+//  SnapKit-TimerApp
+//
+//  Created by detaysoft on 31.07.2022.
+//
+
+import UIKit
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 60
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return pickerView.frame.size.width/3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row) dakika"
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        return self.secondLabel.text = "\(row)"
+    }
+}
--- SnapKit-TimerApp/ViewController.swift
+++ SnapKit-TimerApp/ViewController.swift
@@ -7,93 +7,198 @@
 
 import UIKit
 import AudioToolbox
-
+import SnapKit
+import Spring
 class ViewController: UIViewController {
-
+    let secondView = UIView()
+    let textField = UITextField()
     let nameAppLabel: UILabel = {
-             let label = UILabel()
-             label.text = "Zamanlayıcı"
-             label.font = UIFont.boldSystemFont(ofSize: 40)
-             label.textColor = .black
-
-             label.textAlignment = .center
-             label.translatesAutoresizingMaskIntoConstraints = false
-             return label
-         }()
-
-         let timerLabel: UILabel = {
-             let label = UILabel()
-             label.text = "0"
-             label.font = UIFont.boldSystemFont(ofSize: 80)
-             label.textColor = .black
-             label.textAlignment = .center
-             label.translatesAutoresizingMaskIntoConstraints = false
-             return label
-         }()
-
-         let shapeView: UIImageView = {
-             let imageView = UIImageView()
-             imageView.alpha = 0.1
-             imageView.layer.shadowRadius = 1
-             imageView.layer.shadowColor = UIColor.black.cgColor
-             imageView.layer.shadowOpacity = 1
-             let image = UIImage(named: "ellipse")
-             imageView.image = image
-             imageView.translatesAutoresizingMaskIntoConstraints = false
-             return imageView
-         }()
-
-         let shapeLayer: CAShapeLayer = {
-             let layer = CAShapeLayer()
-
-             return layer
-         }()
-
-         let startButton: UIButton = {
-             let button = UIButton(type: .custom)
-             button.setTitle("Başla", for: .normal)
-             button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
-             button.backgroundColor = .darkText
-             button.layer.cornerRadius = 20
-             button.translatesAutoresizingMaskIntoConstraints = false
-             return button
-         }()
-
-         override func viewDidLoad() {
-             super.viewDidLoad()
-             // Do any additional setup after loading the view.
-             view.backgroundColor = .white
-             view.addSubview(nameAppLabel)
-             view.addSubview(timerLabel)
-             view.addSubview(shapeView)
-             view.addSubview(startButton)
-
-         }
+        let label = UILabel()
+        label.text = "Zamanlayıcı"
+        label.font = UIFont.boldSystemFont(ofSize: 40)
+        label.textColor = .black
+        return label
+    }()
+    var secondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.font = UIFont.boldSystemFont(ofSize: 80)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    let shapeView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.alpha = 1
+        imageView.layer.shadowRadius = 5
+        imageView.layer.shadowColor = UIColor.black.cgColor
+        imageView.layer.shadowOpacity = 0.3
+        let image = UIImage(named: "ellipse")
+        imageView.image = image
+        return imageView
+    }()
+    let shapeLayer: CAShapeLayer = {
+        let layer = CAShapeLayer()
+        return layer
+    }()
+    let startButton: SpringButton = {
+        let button = SpringButton(type: .system)
+        button.setTitle("Başla", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        button.tintColor = .white
+        button.backgroundColor = .systemRed
+        button.layer.cornerRadius = 20
+        button.layer.shadowColor = UIColor.systemGray.cgColor
+        button.layer.shadowOffset = CGSize(width: 0, height: 0)
+        button.layer.shadowRadius = 5
+        button.layer.shadowOpacity = 1
+        return button
+    }()
+    var timer = Timer()
+    var durationTimer = 0
     
-         override func viewDidLayoutSubviews() {
-             NSLayoutConstraint.activate([
-                 nameAppLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-                 nameAppLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        startButtonAnimate()
+        view.backgroundColor = .white
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        let tapGesture = UITapGestureRecognizer(target: self,action: #selector(editValueTimer))
+        secondView.addGestureRecognizer(tapGesture)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.animationCircular()
+    }
+}
 
-                 timerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                 timerLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+// MARK: - Metods
+extension ViewController {
+    @objc func startButtonTapped() {
+        durationTimer = Int(secondLabel.text!)!
+        guard durationTimer > 0 else { return }
+        setBasicAnimation()
+        self.animateButton()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        startButton.isEnabled = false
+    }
+    
+    @objc func timerAction() {
+        durationTimer = Int(secondLabel.text!)!
+        durationTimer -= 1
+        secondLabel.text = "\(durationTimer)"
+        if durationTimer == 0 {
+            self.setAlert()
+            timer.invalidate()
+            startButton.isEnabled = true
+            self.startButton.isHidden = false
+            AudioServicesPlaySystemSound(1000)
+            startButtonAnimate()
+        }
+    }
+    
+    private func animationCircular() {
+        let center = CGPoint(x: shapeView.frame.width / 2, y: shapeView.frame.height / 2 )
+        let endAngle = (-CGFloat.pi / 2)
+        let startAngle = 2 * CGFloat.pi + endAngle
+        let circularPath = UIBezierPath(arcCenter: center,
+                                        radius: 131,
+                                        startAngle: startAngle,
+                                        endAngle: endAngle,
+                                        clockwise: false)
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.lineWidth = 23
+        shapeLayer.fillColor = .none
+        shapeLayer.strokeEnd = 1
+        shapeLayer.lineCap = .round
+        shapeLayer.strokeColor = UIColor.systemRed.cgColor
+        shapeView.layer.addSublayer(shapeLayer)
+    }
+    
+    private func setBasicAnimation() {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = CFTimeInterval(durationTimer)
+        animation.toValue = 0
+        animation.fillMode = .forwards
+        animation.isRemovedOnCompletion = true
+        shapeLayer.add(animation, forKey: nil)
+    }
+    
+    private func setLayout() {
+        view.addSubview(nameAppLabel)
+        view.addSubview(secondLabel)
+        view.addSubview(shapeView)
+        view.addSubview(startButton)
+        view.addSubview(secondView)
+        
+        nameAppLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(80)
+            make.centerX.equalToSuperview()
+        }
+        secondLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        shapeView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.width.equalTo(300)
+            make.height.equalTo(300)
+        }
+        startButton.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(100)
+            make.bottom.equalToSuperview().inset(view.bounds.width / 6)
+            make.height.equalTo(60)
+        }
+        secondView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(200)
+        }
+    }
+    private func animateButton() {
+        UIView.animate(withDuration: 0.6) {
+            self.startButton.frame.origin.y -= self.startButton.frame.height - 200
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.9) {
+            self.startButton.isHidden = true
+        }
+    }
+    
+    @objc func editValueTimer() {
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: .none, action: #selector(pressDone))
+        toolbar.setItems([.flexibleSpace(), doneButton,], animated: true)
+        textField.inputAccessoryView = toolbar
+        textField.isHidden = true
+        secondView.addSubview(textField)
+        textField.inputView = picker
+        textField.becomeFirstResponder()
+    }
+    
+    @objc func pressDone() {
+        self.view.endEditing(true)
+    }
+    private func setAlert() {
+        let alert = UIAlertController(title: "Zaman doldu!", message: nil, preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Tamam", style: .cancel) { _ in
+            print("Tamamlandı")
+        }
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+    private func startButtonAnimate() {
+        startButton.animation = "squeezeUp"
+        startButton.delay = 0.3
+        startButton.duration = 1
+        startButton.damping = 1
+        startButton.animate()
+    }
+}
 
-                 shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                 shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                 shapeView.widthAnchor.constraint(equalToConstant: 300),
-                 shapeView.heightAnchor.constraint(equalToConstant: 300),
-
-                 shapeView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                 shapeView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                 shapeView.widthAnchor.constraint(equalToConstant: 300),
-                 shapeView.heightAnchor.constraint(equalToConstant: 300),
-
-                 startButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80),
-                 startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                 startButton.widthAnchor.constraint(equalToConstant: 170),
-                 startButton.heightAnchor.constraint(equalToConstant: 60)
-             ])
-         }
-     }
-
-
+  
